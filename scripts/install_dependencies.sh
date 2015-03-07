afed7ed7a15388574a988cdf52b03a751f6143c5
Mohit <mohit.gupta@intelligrape.com>
2015-03-07 09:51:39 +0530

    Git

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rm -f /usr/share/nginx/html/index.html
+sudo rm -f /usr/share/nginx/html/index.html
+sudo rm -Rf /usr/share/nginx/html/scripts
+sudo rm /usr/share/nginx/html/appspec.yml
